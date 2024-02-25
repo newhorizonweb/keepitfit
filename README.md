73b6e96258ede99b5d850b5bd57a4540f9b9b6ec
Uverit <web.newhorizon@gmail.com>
2024-02-25 22:05:10 +0100

    v0.1.1 - Search Bar Animation, Design Updates
    Implemented a typewriter animation on the main page's search bar using the "Typed.js" library.
    
    Added media queries to the main page.
    
    Implemented minor "glass" design changes (inner shadow, frost texture).
    Added an animation to the search button (magnifying glass icon) when the user input is valid.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Keep It Fit
+
+Your ultimate app for accessing comprehensive nutritional data.
+
+## Technologies
+
+- Code
+    - React 18.2.0
+        - TypeScript
+        - React Router 6.22.1
+        - Framer Motion 11.0.6 - Library
+    - Typed.js 2.1.0 - Library
+    - GH-Pages 6.1.1
+    - CSS 3
+
+- Graphics & Icons
+    - SVG
+    - Figma
+    - Adobe Illustrator
+    - Gimp
+
+## License
+
+Keep It Fit by Uverit, All Rights Reserved.
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,8 +1,9 @@
 
 
 
-import { KeyboardEvent, useContext, useEffect, useState } from 'react';
+import { FormEvent, KeyboardEvent, useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Typed from "typed.js";
 
 import { PageContext } from '../../App';
 import PageTransition from '../app/PageTransition';
@@ -15,21 +16,17 @@ const Main = () => {
     // Page navigation
     const navigate = useNavigate();
 
-
-
-        /* Variables */
-
     // Page Context Variables
     const { urlPath, magGlassIcon } = useContext(PageContext);
-
-    // Search keyword
-    const [searchVal, setSearchVal] = useState("");
 
 
 
         /* Search */
 
-    // Searchbar input value
+    // Search keyword value
+    const [searchVal, setSearchVal] = useState("");
+
+    // Search validation message
     const [inpMsg, setInpMsg] = useState("");
 
     // Check if the search input value is valid
@@ -50,10 +47,36 @@ const Main = () => {
     const maxCharSearch = 128;
     const tooLongMsg = `Use max ${maxCharSearch} characters`;
 
+    // Placeholders (typing animation - typed.js)
+    const typingContents = [
+        "Brown Rice",
+        "Bro^1500ccoli",
+        "Honey",
+        "Pork Chop",
+        "Pork Tenderloin",
+        "Tomato",
+        "Greek Yogurt",
+        "Chicken^500 Breast",
+        "Chic^500kpe^500as",
+        "Apricot",
+        "Roast^500ed Almonds",
+        "Carrots",
+        "Cabbage",
+        
+        "Shrimp",
+        "Maple Syrup",
+        "Pea^1000nuts",
+        "Peaches",
+        "Oregano",
+        "Avodado",
+        "Asparagus"
 
-    
+    ];
 
-    
+    const [showPlaceHolder, setShowPlaceholder] = useState(true);
+    const [isFocused, setIsFocused] = useState(false);
+
+
 
         /* Functions */
 
@@ -69,11 +92,36 @@ const Main = () => {
         
     }
 
-    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    const searchInpKeyDown = (e: KeyboardEvent<HTMLParagraphElement>) => {
+        
         if (e.key === "Enter"){
             goToDetails();
         }
     }
+
+    const searchInpChange = (e: FormEvent<HTMLParagraphElement>) => {
+        
+        const text = e.currentTarget.textContent;
+
+        if (text !== null) {
+            setSearchVal(text);
+        }
+        
+        if (text != ""){
+            setShowPlaceholder(false);
+        } else {
+            setShowPlaceholder(true);
+        }
+
+    }
+
+    const searchInpFocus = () => {
+        setIsFocused(true);
+    };
+    
+    const searchInpBlur = () => {
+        setIsFocused(false);
+    };
 
     // Update the search value (reactive searchVal -> localstorage)
     useEffect(() => {
@@ -89,7 +137,7 @@ const Main = () => {
                 setInpMsg(tooLongMsg);
                 break;
 
-                case !regex.test(searchVal):
+            case !regex.test(searchVal):
                 const invalidChars = new Set(searchVal.match(/[^a-zA-Z0-9,.()\- \p{L}]/gu));
                 const invalidCharList = Array.from(invalidChars).join("");
                 setInpMsg(
@@ -109,6 +157,33 @@ const Main = () => {
         }
 
     }, [searchVal]);
+
+    const searchPhRef = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(searchPhRef.current, {
+            strings: typingContents,
+            
+            startDelay: 0,
+            typeSpeed: 200,
+            backSpeed: 70,
+            backDelay: 2000,
+
+            smartBackspace: true,
+            loop: true,
+            showCursor: true,
+            cursorChar: "",
+        });
+
+        return () => {
+            typed.destroy();
+        };
+
+    }, []);
+
+
+
+
     
     return (
         <section className="main-page wrapper">
@@ -116,10 +191,11 @@ const Main = () => {
             <div className="main-content">
                 <img className="logo" src={ Logo } alt="Keep It Fit Logo" />
 
-                <div className="search-bar glass">
+                {/* <div className="search-bar glass">
                     <input
                         type="text"
-                        placeholder="Tomato..."
+                        
+
                         onChange={ (e) => setSearchVal(e.currentTarget.value) }
                         onKeyDown={ (e) => handleKeyDown(e) }
                     />
@@ -129,6 +205,36 @@ const Main = () => {
                     onClick={ goToDetails }>
                         { magGlassIcon }
                     </button>
+                </div> */}
+
+                <div className="search-bar glass">
+
+                    <div className="search-inner">
+
+                        <div className={`
+                            search-placeholder
+                            ${ showPlaceHolder ? 'show-placeholder' : ''}
+                            ${ isFocused ? 'hide-cursor' : ''}
+                        `}>
+                            <span ref={searchPhRef}
+                                className="search-placeholder-txt">
+                            </span>
+                        </div>
+
+                        <p className="search-input"
+                            onInput={(e) => searchInpChange(e)}
+                            onKeyDown={ (e) => searchInpKeyDown(e) }
+                            onFocus={ searchInpFocus }
+                            onBlur={ searchInpBlur }
+                            contentEditable="true">
+                        </p>
+                    </div>
+
+                    <button className={`${ isValid ? 'valid-search' : '' }`}
+                        onClick={ goToDetails }>
+                        { magGlassIcon }
+                    </button>
+
                 </div>
 
                 <p className={
